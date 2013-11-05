98ae78cd75a3e59d800cdff4f12ac25aa70ce0c1
Louis Rousseau <louisrousseau@gmail.com>
2013-11-05 03:30:37 -0500

    Update BoardCommonConfig.mk
    Fixed TARGET_KERNEL_CONFIG, build did not make it very far with the cyanogenmod value.

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -32,9 +32,9 @@ COMMON_GLOBAL_CFLAGS += -DCAMERA_WITH_CITYID_PARAM
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
 ifeq ($(TARGET_VOICE_TECH), cdma)
-TARGET_KERNEL_CONFIG := cyanogenmod_t0ltecdma_defconfig
+TARGET_KERNEL_CONFIG := custom_t0ltecdma_defconfig
 else
-TARGET_KERNEL_CONFIG := cyanogenmod_t0lte_defconfig
+TARGET_KERNEL_CONFIG := custom_t0lte_defconfig
 endif
 
 # Recovery
