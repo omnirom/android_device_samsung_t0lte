cd4bfeeeb92af4cd4a980ea2ff17dc9326af90a5
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-12-14 21:13:24 +0530

    t0lte: disable voicecall route on init & leaving incall state
    Change-Id: Ida0a5d2066fe45a1f929188dc67b786d43a02a9b

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -261,7 +261,7 @@ static void in_update_aux_channels(struct m0_stream_in *in, effect_handle_t effe
 
 /* The enable flag when 0 makes the assumption that enums are disabled by
  * "Off" and integers/booleans by 0 */
-static int set_bigroute_by_array(struct mixer *mixer, struct route_setting *route,
+static int set_voicecall_route_by_array(struct mixer *mixer, struct route_setting *route,
                               int enable)
 {
     struct mixer_ctl *ctl;
@@ -683,6 +683,11 @@ static void select_mode(struct m0_audio_device *adev)
             }
 
             force_all_standby(adev);
+
+            /* Disable voicecall route */
+            ALOGE("Disabling voicecall route");
+            set_voicecall_route_by_array(adev->mixer, voicecall_default_disable, 1);
+
             select_output_device(adev);
             select_input_device(adev);
         }
@@ -769,36 +774,36 @@ static void select_output_device(struct m0_audio_device *adev)
 
         if (headset_on || headphone_on || speaker_on || earpiece_on) {
             ALOGD("%s: set voicecall route: voicecall_default", __func__);
-            set_bigroute_by_array(adev->mixer, voicecall_default, 1);
+            set_voicecall_route_by_array(adev->mixer, voicecall_default, 1);
         } else {
             ALOGD("%s: set voicecall route: voicecall_default_disable", __func__);
-            set_bigroute_by_array(adev->mixer, voicecall_default_disable, 1);
+            set_voicecall_route_by_array(adev->mixer, voicecall_default_disable, 1);
         }
 
         if (speaker_on || earpiece_on || headphone_on) {
             ALOGD("%s: set voicecall route: default_input", __func__);
-            set_bigroute_by_array(adev->mixer, default_input, 1);
+            set_voicecall_route_by_array(adev->mixer, default_input, 1);
         } else {
             ALOGD("%s: set voicecall route: default_input_disable", __func__);
-            set_bigroute_by_array(adev->mixer, default_input_disable, 1);
+            set_voicecall_route_by_array(adev->mixer, default_input_disable, 1);
         }
 
         if (headset_on) {
             ALOGD("%s: set voicecall route: headset_input", __func__);
-            set_bigroute_by_array(adev->mixer, headset_input, 1);
+            set_voicecall_route_by_array(adev->mixer, headset_input, 1);
         } else {
             ALOGD("%s: set voicecall route: headset_input_disable", __func__);
-            set_bigroute_by_array(adev->mixer, headset_input_disable, 1);
+            set_voicecall_route_by_array(adev->mixer, headset_input_disable, 1);
         }
 
         if (bt_on) {
             ALOGD("%s: set voicecall route: bt_input", __func__);
-            set_bigroute_by_array(adev->mixer, bt_input, 1);
+            set_voicecall_route_by_array(adev->mixer, bt_input, 1);
             ALOGD("%s: set voicecall route: bt_output", __func__);
-            set_bigroute_by_array(adev->mixer, bt_output, 1);
+            set_voicecall_route_by_array(adev->mixer, bt_output, 1);
         } else {
             ALOGD("%s: set voicecall route: bt_disable", __func__);
-            set_bigroute_by_array(adev->mixer, bt_disable, 1);
+            set_voicecall_route_by_array(adev->mixer, bt_disable, 1);
         }
         set_incall_device(adev);
     }
@@ -2693,11 +2698,11 @@ static int adev_set_parameters(struct audio_hw_device *dev, const char *kvpairs)
         if (strcmp(value, "true") == 0) {
             ALOGE("%s: enabling two mic control", __func__);
             /* sub mic */
-            set_bigroute_by_array(adev->mixer, noise_suppression, 1);
+            set_voicecall_route_by_array(adev->mixer, noise_suppression, 1);
         } else {
             ALOGE("%s: disabling two mic control", __func__);
             /* sub mic */
-            set_bigroute_by_array(adev->mixer, noise_suppression_disable, 1);
+            set_voicecall_route_by_array(adev->mixer, noise_suppression_disable, 1);
         }
     }
 
@@ -3191,10 +3196,6 @@ static int adev_open(const hw_module_t* module, const char* name,
         return -EINVAL;
     }
 
-    /* +30db boost for mics */
-    adev->mixer_ctls.mixinl_in1l_volume = mixer_get_ctl_by_name(adev->mixer, "MIXINL IN1L Volume");
-    adev->mixer_ctls.mixinl_in2l_volume = mixer_get_ctl_by_name(adev->mixer, "MIXINL IN2L Volume");
-
     ret = adev_config_parse(adev);
     if (ret != 0)
         goto err_mixer;
@@ -3205,6 +3206,13 @@ static int adev_open(const hw_module_t* module, const char* name,
     adev->out_device = AUDIO_DEVICE_OUT_SPEAKER;
     adev->in_device = AUDIO_DEVICE_IN_BUILTIN_MIC & ~AUDIO_DEVICE_BIT_IN;
     select_devices(adev);
+
+    /* +30db boost for mics */
+    adev->mixer_ctls.mixinl_in1l_volume = mixer_get_ctl_by_name(adev->mixer, "MIXINL IN1L Volume");
+    adev->mixer_ctls.mixinl_in2l_volume = mixer_get_ctl_by_name(adev->mixer, "MIXINL IN2L Volume");
+
+    /* Disable voicecall route */
+    set_voicecall_route_by_array(adev->mixer, voicecall_default_disable, 1);
 
     adev->pcm_modem_dl = NULL;
     adev->pcm_modem_ul = NULL;
