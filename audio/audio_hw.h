0870ae642a42b23edf11af61bc39b6fafbc86969
markcs <mcampbellsmith@gmail.com>
2014-02-18 12:31:32 +0530

    t0lte: update audio hal
    Bring in following from i9305 audio hal:
    - Fixes headphone sometimes not enabling
    - Increased mic gain
    - Cleaned up audio dock configuration
    - Increase speaker volume during voice call
    - Removed invalid mic controls
    
    Change-Id: I4c79d078f571a08bc22ee9b11a2f92f6539876cd

--- audio/audio_hw.h
+++ audio/audio_hw.h
@@ -188,7 +188,6 @@ struct route_setting voicecall_default_disable[] = {
 
 struct route_setting default_input[] = {
     { .ctl_name = "Main Mic Switch", .intval = 1, },
-    { .ctl_name = "MainMicBias Mode", .intval = 1, },
     { .ctl_name = "IN2L Volume", .intval = 28, },
     { .ctl_name = "MIXINL IN2L Switch", .intval = 1, },
     { .ctl_name = "MIXINL IN2L Volume", .intval = 0, },
@@ -226,7 +225,7 @@ struct route_setting headset_input[] = {
     { .ctl_name = "MIXINL IN2L Switch", .intval = 0, },
     { .ctl_name = "MIXINR IN1R Switch", .intval = 0, },
     { .ctl_name = "Headset Mic Switch", .intval = 1, },
-    { .ctl_name = "IN1L Volume", .intval = 18, },
+    { .ctl_name = "IN1L Volume", .intval = 28, },
     { .ctl_name = "MIXINL IN1L Switch", .intval = 1, },
     { .ctl_name = "MIXINL IN1L Volume", .intval = 0, },
     { .ctl_name = "AIF1ADC1 HPF Mode", .intval = 1, },
@@ -298,5 +297,15 @@ struct route_setting bt_disable[] = {
     { .ctl_name = "AIF2DAC2L Mixer AIF2 Switch", .intval = 0, },
     { .ctl_name = "AIF1ADC1R Mixer AIF2 Switch", .intval = 0, },
     { .ctl_name = "AIF1ADC1L Mixer AIF2 Switch", .intval = 0, },
+    { .ctl_name = NULL, },
+};
+
+struct route_setting speaker_output[] = {
+    { .ctl_name = "Speaker Boost Volume", .intval = 5, },
+    { .ctl_name = NULL, },
+};
+
+struct route_setting speaker_output_disable[] = {
+    { .ctl_name = "Speaker Boost Volume", .intval = 4, },
     { .ctl_name = NULL, },
 };
