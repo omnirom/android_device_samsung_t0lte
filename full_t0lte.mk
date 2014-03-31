33b692a5119a571b085d872a6234b7e6ea7ea7ef
UtkarshGupta <utkarsh.eminem@gmail.com>
2014-03-31 23:33:16 +0530

    t0lte: consolidate variants
    Change-Id: Id007aed7f49684b2ccafd7f786abfb16d0aa3a69

--- full_t0lte.mk
+++ full_t0lte.mk
@@ -25,7 +25,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
-$(call inherit-product, device/samsung/t0lte/t0lte.mk)
+$(call inherit-product, device/samsung/t0lte/device.mk)
 
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_t0lte
