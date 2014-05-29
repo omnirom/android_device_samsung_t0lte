854b06193a2ab9f89d83c047cac0346c713411bd
Utkarsh Gupta <utkarsh.eminem@gmail.com>
2014-05-29 20:53:59 +0530

    Add t0ltecan
    Change-Id: I3e4003b55648c1e27a42e9f82468a591937c6676

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,7 +24,7 @@
 TARGET_RECOVERY_FSTAB := device/samsung/t0lte/rootdir/fstab.smdk4x12
 
 # assert
-TARGET_OTA_ASSERT_DEVICE := t0lte,t0ltexx,GT-N7105,t0ltedv,GT-N7105T,t0lteatt,SGH-I317,t0ltetmo,SGH-T889
+TARGET_OTA_ASSERT_DEVICE := t0lte,t0ltexx,GT-N7105,t0ltedv,GT-N7105T,t0lteatt,SGH-I317,t0ltetmo,SGH-T889,t0ltecan,t0ltevl,SGH-I317M
 
 # inherit from the proprietary version
 -include vendor/samsung/t0lte/BoardConfigVendor.mk
